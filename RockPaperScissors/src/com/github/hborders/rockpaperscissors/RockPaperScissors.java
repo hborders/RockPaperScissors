89055a48366850c4efe2e058ad992e3cf7d42bf4
hborders <hborders@mail.win.org>
2009-12-19 16:07:09 -0600

    Simple spike of error checking based on arguments.

---
+++ RockPaperScissors/src/com/github/hborders/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,18 @@
+package com.github.hborders.rockpaperscissors;
+
+import java.util.Arrays;
+
+public class RockPaperScissors {
+	public static void main(String[] args) {
+		if (args.length == 2) {
+		
+		} else if (args.length == 4) {
+			
+		} else {
+			System.out.println("Usage:");
+			System.out.println("java RockPaperScissors -to <win count>");
+			System.out.println("java RockPaperScissors -to <win count> -by <win margin>");
+			System.out.println("java RockPaperScissors -bestof <game count>");
+		}
+	}
+}
