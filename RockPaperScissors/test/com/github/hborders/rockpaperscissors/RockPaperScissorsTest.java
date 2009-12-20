34ed0955aaf9decf61bd986ed7741c52377fd392
hborders <hborders@mail.win.org>
2009-12-20 00:03:54 -0600

    I failed to track all my past changes to my tests.  Oops. Added test for invalid -to or -bestof options.  feature not yet implemented.

---
+++ RockPaperScissors/test/com/github/hborders/rockpaperscissors/RockPaperScissorsTest.java
@@ -0,0 +1,68 @@
+package com.github.hborders.rockpaperscissors;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RockPaperScissorsTest {
+
+	private UsagePrinter mockUsagePrinter;
+	private RockPaperScissors testObject;
+
+	@Before
+	public void setUp() {
+		mockUsagePrinter = mock(UsagePrinter.class);
+		testObject = new RockPaperScissors(mockUsagePrinter);
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_arguments_length_is_1() {
+		testObject.play(new String[1]);
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_arguments_length_is_3() {
+		testObject.play(new String[3]);
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_arguments_length_is_greater_than_4() {
+		testObject.play(new String[5]);
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_two_argument_array_first_argument_is_not_to_or_bestof() {
+		testObject.play(new String[] { "foo", "bar" });
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_two_argument_array_second_argument_is_not_integer_greater_than_0() {
+		testObject.play(new String[] { "-to", "0" });
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_four_argument_array_first_argument_is_not_to_or_bestof() {
+		testObject.play(new String[] { "foo", "bar", "foo", "foo" });
+
+		verify(mockUsagePrinter).printUsage();
+	}
+
+	@Test
+	public void play_calls_UsagePrinter_when_four_argument_array_third_argument_is_not_by() {
+		testObject.play(new String[] { "-to", "bar", "foo", "foo" });
+
+		verify(mockUsagePrinter).printUsage();
+	}
+}
