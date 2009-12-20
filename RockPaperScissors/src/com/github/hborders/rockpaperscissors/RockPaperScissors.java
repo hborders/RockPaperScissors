399e4d671b18294bfc9148308f1a8d09ee6a9108
hborders <hborders@mail.win.org>
2009-12-19 23:48:39 -0600

    Wrote tests for UsagePrinter.  Obviously, still need to verify that valid data has been entered for proper argument lengths.

--- RockPaperScissors/src/com/github/hborders/rockpaperscissors/RockPaperScissors.java
+++ RockPaperScissors/src/com/github/hborders/rockpaperscissors/RockPaperScissors.java
@@ -2,6 +2,16 @@ package com.github.hborders.rockpaperscissors;
 
 public class RockPaperScissors {
 	public static void main(String[] args) {
+		new RockPaperScissors(new UsagePrinter()).play(args);
+	}
+
+	private final UsagePrinter usagePrinter;
+
+	public RockPaperScissors(UsagePrinter usagePrinter) {
+		this.usagePrinter = usagePrinter;
+	}
+
+	public void play(String[] args) {
 		if (args.length == 0) {
 
 		} else if (args.length == 2) {
@@ -9,7 +19,7 @@ public class RockPaperScissors {
 		} else if (args.length == 4) {
 
 		} else {
-			new UsagePrinter().printUsage();
+			usagePrinter.printUsage();
 		}
 	}
 }
