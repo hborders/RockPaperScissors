5812a54d7e1d7dad601087e1eb32ef65cd1e4cf1
hborders <hborders@mail.win.org>
2009-12-19 23:17:25 -0600

    Refactored usage printing into a separate class.  Will include Mockito libs and write a test to verify usage.

--- RockPaperScissors/src/com/github/hborders/rockpaperscissors/RockPaperScissors.java
+++ RockPaperScissors/src/com/github/hborders/rockpaperscissors/RockPaperScissors.java
@@ -1,18 +1,15 @@
 package com.github.hborders.rockpaperscissors;
 
-import java.util.Arrays;
-
 public class RockPaperScissors {
 	public static void main(String[] args) {
-		if (args.length == 2) {
-		
+		if (args.length == 0) {
+
+		} else if (args.length == 2) {
+
 		} else if (args.length == 4) {
-			
+
 		} else {
-			System.out.println("Usage:");
-			System.out.println("java RockPaperScissors -to <win count>");
-			System.out.println("java RockPaperScissors -to <win count> -by <win margin>");
-			System.out.println("java RockPaperScissors -bestof <game count>");
+			new UsagePrinter().printUsage();
 		}
 	}
 }
